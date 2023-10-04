87053efdcb1e7a20a6b4ad47cb9bfc74ef5cf79f
Sarah H <sarah@cleancodecompany.com>
2023-10-04 17:04:29 +0100

    feat: added Reactstrap imports and created a basic Navbar

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> This is a test </Text> <StatusBar style="auto" />
+      <Text> This is a tes </Text> <StatusBar style="auto" />
     </View>
   );
 }
