704fd50a86c1d7315e9f3f4eba081bf49a2d92a1
Sarah H <sarah@cleancodecompany.com>
2023-11-09 20:39:15 +0000

    feat(api): database migrations progress

--- api/migrations/20231109212510_dbSetUp.ts
+++ api/migrations/20231109212510_dbSetUp.ts
@@ -4,6 +4,21 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
    await knex.schema.createTable("Users", (userTable) => {
         userTable.increments("userID").unique().notNullable();
+        userTable.string("foreman").notNullable();
+        userTable.string("surname").notNullable();
+        userTable.integer("number").unique().notNullable();
+        userTable.string("email").unique().notNullable();
+        userTable.string("address").notNullable();
+        userTable.string("town").notNullable();
+        userTable.string("county").notNullable();
+        userTable.string("eircode").unique().notNullable();
+        userTable.enum("education", ["Leaving Cert", "Level 5", "Level 6", "Level 7", "Level 8", "Level 9", "Level 10", "Other"]).notNullable();
+        userTable.enum("experience", ["General Operative", "Site Assisatant", "Supervisor", "Director", "Student", "Other" ]).notNullable();
+        userTable.enum("certs", ["Safe Pass", "Manual Handling", "First Aid"]).notNullable();
+        userTable.boolean("driving").notNullable();
+        userTable.enum("position",["General Operative", "Site Assisatant", "Supervisor", "Director", "Student", "Other"]).notNullable();
+        userTable.string("site").notNullable();
+
     })
 }
 
