4104ee73fe8390880c3b962b6a9fb3004b5d1ca8
Sarah H <sarah@cleancodecompany.com>
2023-11-16 20:59:09 +0000

    feat(webapp): add user form validation  progress

---
+++ webapp/src/validation/teamValidation.tsx
@@ -0,0 +1,17 @@
+import * as yup from "yup";
+
+const teamSchema = yup.object().shape({
+  name: yup.string().min(2).max(10).required(),
+  surname: yup.string().min(2).max(10).required(),
+  number: yup.string().min(10).required(),
+  email: yup.string().email().lowercase().required(),
+  address: yup.string().min(2).max(10).required(),
+  townCity: yup.string().min(10).required(),
+  county: yup.string().email().required(),
+  eircode: yup.string().min(2).max(10).required(),
+  //   experience: yup.string().min(10).required(),
+  //   education: yup.string().email().required(),
+  //   training: yup.string().min(10).required(),
+  //   drive: yup.enmum().email().required(), ????
+});
+export default teamSchema;
