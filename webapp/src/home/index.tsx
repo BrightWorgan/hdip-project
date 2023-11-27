df7c9003ad1f8b536457d060f27e74f603b7280d
Sarah H <sarah@cleancodecompany.com>
2023-11-27 22:13:43 +0000

    chore(webapp): moved home page text and edited carosel

--- webapp/src/home/index.tsx
+++ webapp/src/home/index.tsx
@@ -2,16 +2,8 @@ import HomeImages from "./carosel";
 
 const Home = () => {
   return (
-    <div>
-      {/* <br></br> */}
-      <div>
-        <HomeImages />
-      </div>
-      <h2>Welcome to Sheet Scribe</h2>
-      <h4>For all of your Traceable Heritage needs</h4>
-      <br />
-      <div className="ss-spacer"></div>
-      <br />
+    <div style={{ height: "100vh" }}>
+      <HomeImages />
     </div>
   );
 };
