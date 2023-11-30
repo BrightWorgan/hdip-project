f37613efa4edfe1949ee668e1d7e7822048ba561
Sarah H <sarah@cleancodecompany.com>
2023-11-30 18:07:23 +0000

    chore: more  sonarcloud code smells clean up

--- webapp/src/projects/reports/sampleTableTab.tsx
+++ webapp/src/projects/reports/sampleTableTab.tsx
@@ -1,14 +1,13 @@
 import { Container } from "reactstrap";
-// import ReportHeader from "./reportHeader";
 const SampleTable = () => {
   return (
     <div>
-      {/* <ReportHeader /> */}
       <br></br>
-      <h1>Reports page / Sample Table page is working</h1>
-      <h2>Needs table!</h2>
       <div>
-        <Container></Container>
+        <Container>
+          <h1>Reports page / Sample Table page is working</h1>
+          <h2>Needs table!</h2>
+        </Container>
       </div>
     </div>
   );
