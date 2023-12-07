836e08d89a1ce506419c0a11f5032bdee7188ad7
Sarah H <sarah@cleancodecompany.com>
2023-12-07 17:24:32 +0000

    feat(webapp) added decoed user details to user profile page

--- webapp/src/userProfile/profileCard.tsx
+++ webapp/src/userProfile/profileCard.tsx
@@ -10,14 +10,10 @@ import {
   Button,
   CardHeader,
 } from "reactstrap";
+import util from "../util";
 
-import {
-  lightColors,
-  darkColors,
-  // @ts-ignore
-} from "react-floating-action-button";
-
-const ProfileCard = () => {
+const ProfileCard = (props: any) => {
+  const user = util.getUser();
   return (
     <Row>
       <Col xs="4">
@@ -29,11 +25,7 @@ const ProfileCard = () => {
                   alt="user profile image"
                   src="src/assets/meZeusandSkadi.jpg"
                   height="100%"
-                  //   width="25px"
                   style={{
-                    // width: "5%",
-                    // width: "25px",
-                    //   height: "100%",
                     borderRadius: "50%",
                     overflow: "hidden",
                     borderWidth: 3,
@@ -44,29 +36,35 @@ const ProfileCard = () => {
               <Col
                 style={{
                   display: "flex",
-                  //   justifyContent: "center",
                   alignItems: "center",
                 }}
               >
-                <h2>Sarah Halford</h2>
+                <h2>{user?.forename + " " + user?.surname}</h2>
               </Col>
             </Row>
           </CardHeader>
           <CardBody>
-            <CardTitle tag="h5">Name: </CardTitle>
+            <CardTitle tag="h5">
+              Name:
+              {" " +
+                user?.forename +
+                " " +
+                user?.surname?.substring(0, 1) +
+                "."}
+            </CardTitle>
             <CardSubtitle className="mb-2 text-muted" tag="h6">
-              Position:
+              Position: {" " + user?.position}
             </CardSubtitle>
             <CardText>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-              Donec qu
+              {user?.education}
+              consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+              Aenean massa. Cum sociis natoque penatibus et magnis dis
+              parturient montes, nascetur ridiculus mus. Donec qu
             </CardText>
             <Button
               className="ss-info-btn"
               target="_blank"
-              href="https://www.linkedin.com/in/sarah-halford-369b1414b/"
+              href="https://www.linkedin.com/"
             >
               LinkedIn
             </Button>
