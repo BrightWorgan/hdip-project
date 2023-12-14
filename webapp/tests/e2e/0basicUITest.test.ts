08a8bd699fe5c055dd34537bad3c93000cba3648
Sarah H <sarah@cleancodecompany.com>
2023-12-14 21:02:05 +0000

    feat(test):added more e-2-e tests

--- webapp/tests/e2e/0basicUITest.test.ts
+++ webapp/tests/e2e/0basicUITest.test.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
   test.describe('Login and basic UI structure', () => {
     test('Invalid login test', async ({ page }) => {
         await test.step('load page', async () => {
-          await page.goto('http://45.77.59.59/');
+          await page.goto('baseURL' );
         });        
       await test.step('login with incorrect email', async () => {
         const emailInput = await page.getByPlaceholder('Email Addresss');
@@ -38,7 +38,7 @@ import { test, expect } from '@playwright/test';
 
     test('Sucessful login test', async ({ page }) => {
       await test.step('load page', async () => {
-        await page.goto('http://45.77.59.59/');
+        await page.goto('baseURL' );
       });
 
       await test.step('login with correct email', async () => {
@@ -70,11 +70,10 @@ import { test, expect } from '@playwright/test';
       });
 
       });
-  });
 
   test('Base UI structure', async ({ page }) => {
     await test.step('load page', async () => {
-      await page.goto('http://45.77.59.59/');
+      await page.goto('baseURL' );
     });
 
     await test.step('login with correct email and password', async () => {
@@ -133,11 +132,10 @@ import { test, expect } from '@playwright/test';
      
     });
   });
-
   
   test('Home Page structure', async ({ page }) => {
     await test.step('load page', async () => {
-      await page.goto('http://45.77.59.59/');
+      await page.goto('baseURL' );
     });
 
     await test.step('login with correct email and password', async () => {
@@ -166,25 +164,192 @@ import { test, expect } from '@playwright/test';
 
   test('Project Page structure', async ({ page }) => {
     await test.step('load page', async () => {
-      await page.goto('http://45.77.59.59/');
+      await page.goto('baseURL' );
     });
 
     await test.step('login with correct email and password', async () => {
       await page.getByPlaceholder('Email Addresss').click();
-      await page.getByPlaceholder('Email Addresss').type('edwardtest@test.com');
+      await page.getByPlaceholder('Email Addresss').type('test@test.com');
       await page.getByPlaceholder('Password...').click();
-      await page.getByPlaceholder('Password...').type('Yc(bLS*od+iX');
+      await page.getByPlaceholder('Password...').type('Test');
       await page.getByRole('button', { name: 'Login' }).click();
     });
 
-    await test.step('Check Carosol has visable buttons', async () => {
-      const prevBtn = await page.getByRole('button', { name: 'Previous' })
-      await expect(prevBtn).toBeVisible()
-      await prevBtn.hover();
-     
+    await test.step('Navigate to Project Page', async () => {
+      await page.getByRole('link', { name: 'Project' }).click();
+    });
+
+    await test.step('Project Page FAB UI', async () => {
+      const fabMenu = await page.getByRole('button', { name: 'Menu ' });
+      await expect(fabMenu).toBeVisible();
+      await fabMenu.hover();
+      await fabMenu.click();
+      await page.getByRole('button', { name: 'Add Project ' }).hover();
+      await page.getByRole('button', { name: 'Edit Project ' }).hover();
+      await page.getByRole('button', { name: 'Delete Project ' }).hover();
+    });
+
+    await test.step('Confirm Add Project Form layout', async () =>{
+      const fabMenu = await page.getByRole('button', { name: 'Menu ' });
+      await fabMenu.hover();
+      await fabMenu.click();
+      await page.getByRole('button', { name: 'Add Project ' }).click();
+      await page.getByRole('heading', { name: 'Add a new Project' }).hover();
+      await page.getByText('Project Name').hover();
+      await page.getByPlaceholder('Project Name').hover();
+      await page.getByText('Location', { exact: true }).hover();
+      await page.getByText('Start Date').hover();
+      await page.getByText('Primary Director Name').hover();
+      await page.getByText('Contract TypePrivatePublicField SchoolResearch').hover();
+      await page.getByLabel('Contract Type').selectOption('Public');
+      await page.getByLabel('Contract Type').selectOption('Field School');
+      await page.getByLabel('Contract Type').selectOption('Research');
+      await page.getByText('licence #:', { exact: true }).hover();
+      await page.getByText('Description', { exact: true }).hover();
+      await page.getByRole('button', { name: 'Cancel' }).hover();
+      await page.getByRole('button', { name: 'Submit' }).hover();
+      await page.getByRole('button', { name: 'Cancel' }).click();
+    })
+
+      
+  });
+
+  test('Team Page structure', async ({ page }) => {
+    await test.step('load page', async () => {
+      await page.goto('baseURL' );
+    });
+
+    await test.step('login with correct email and password', async () => {
+      await page.getByPlaceholder('Email Addresss').click();
+      await page.getByPlaceholder('Email Addresss').type('test@test.com');
+      await page.getByPlaceholder('Password...').click();
+      await page.getByPlaceholder('Password...').type('Test');
+      await page.getByRole('button', { name: 'Login' }).click();
+    });
+
+    await test.step('Navigate to Team Page', async () => {
+      await page.getByRole('link', { name: 'Team' }).click();
+    });
+
+    await test.step('Team Page Table  Confirm Column names anf FAB UI', async () => {
+      
+      // Team Table column names
+      await page.getByRole('cell', { name: 'First Name' }).hover();
+      await page.getByRole('cell', { name: 'Last Name' }).hover();
+      await page.getByRole('cell', { name: 'Mobile Number' }).hover();
+      await page.getByRole('cell', { name: 'Email Address' }).hover();
+      await page.getByRole('cell', { name: 'Home Address' }).hover();
+      await page.getByRole('cell', { name: 'Experience' }).hover();
+      await page.getByRole('cell', { name: 'Education' }).hover();
+      await page.getByRole('cell', { name: 'Training Certs' }).hover();
+      await page.getByRole('cell', { name: 'Drive' }).hover();
+      await page.getByRole('cell', { name: 'Company Position' }).hover();
+      await page.getByRole('cell', { name: 'Current Site' }).hover();
+
+      // FAB
+      const fabMenu = await page.getByRole('button', { name: 'Menu ' });
+      await expect(fabMenu).toBeVisible();
+      await fabMenu.hover();
+      await fabMenu.click();
+      await page.getByRole('button', { name: 'Add Team ' }).hover();
+      await page.getByRole('button', { name: 'Edit Team ' }).hover();
+      await page.getByRole('button', { name: 'Delete Team ' }).hover();
+    });
+  });
+  
+  test('Profile Page structure', async ({ page }) => {
+    await test.step('load page', async () => {
+      await page.goto('baseURL' );
+    });
+
+    await test.step('login with correct email and password', async () => {
+      await page.getByPlaceholder('Email Addresss').click();
+      await page.getByPlaceholder('Email Addresss').type('test@test.com');
+      await page.getByPlaceholder('Password...').click();
+      await page.getByPlaceholder('Password...').type('Test');
+      await page.getByRole('button', { name: 'Login' }).click();
+    });
+
+    await test.step('load page', async () => {
+      // navigate to user profile page
+      await page.getByRole('link', { name: 'User Profile' }).hover();
+      await page.getByRole('link', { name: 'User Profile' }).click();
+
+      // user details
+      await page.getByRole('heading', { name: 'Test Test' }).hover();
+      await page.getByRole('heading', { name: 'Name: Test T.' }).hover();
+      await page.getByRole('heading', { name: 'Position: Director' }).hover();
+      await page.getByText('Education: PHD / Level').hover();
+      await page.getByText('Experience: Director').hover();
+      await page.getByText('Adddress:').hover();
+      await page.getByText('Training: Safe Pass').hover();
+      await page.getByText('Drive:').hover();
+      await page.getByText('Company Position: Director').hover();
+      await page.getByRole('link', { name: 'LinkedIn' }).hover();
+      await page.getByRole('link', { name: 'Twitter' }).hover();
+      
+      // finds table
+      const tableHeader =  await page.getByRole('heading', { name: 'Finds Register:' });
+     await expect(tableHeader).toBeVisible();
+     await tableHeader.hover();
+
+     const findNum =   await page.getByRole('cell', { name: 'Finds Number' });
+     await expect(findNum).toBeVisible();
+     await findNum.hover();
+
+     const contextNum =  await page.getByRole('cell', { name: 'Context Number' });
+     await expect(contextNum).toBeVisible();
+     await contextNum.hover();
+
+     const fillNum =  await page.getByRole('cell', { name: 'Fill Number' });
+     await expect(fillNum).toBeVisible();
+     await fillNum.hover();
+
+     const des =  await page.getByRole('cell', { name: 'Description' });
+     await expect(des).toBeVisible();
+     await des.hover();
+
+     const material =   await page.getByRole('cell', { name: 'Material' });
+     await expect(material).toBeVisible();
+     await material.hover();
+
+     const contextNum =  await page.getByRole('cell', { name: 'Context Number' });
+     await expect(contextNum).toBeVisible();
+     await contextNum.hover();
+
+     const photograph =  await page.getByRole('cell', { name: 'Description' });
+     await expect(photograph).toBeVisible();
+     await photographdes.hover();
+ 
+    
+      await page.getByRole('cell', { name: 'Photograph' }).hover();
+      await page.getByRole('cell', { name: 'Bagged' }).hover();
+      await page.getByRole('cell', { name: 'Date' }).hover();
+      await page.getByRole('cell', { name: 'Found by' }).hover();
 
     });
     
-  });
+    
+  })
+
+});
+   
+  //   await page.getByRole('button', { name: 'Menu ' }).click();
+  //   await page.getByRole('button', { name: 'Add Project ' }).click();
+  //   await page.getByRole('heading', { name: 'Add a new Project' }).click();
+  //   await page.getByText('Project Name').click();
+  //   await page.getByPlaceholder('Project Name').click();
+  //   await page.getByText('Location', { exact: true }).click();
+  //   await page.getByText('Start Date').click();
+  //   await page.getByPlaceholder('/10/2023').fill('2023-12-14');
+  //   await page.getByText('Primary Director Name').click();
+  //   await page.getByText('Contract TypePrivatePublicField SchoolResearch').click();
+  //   await page.getByLabel('Contract Type').selectOption('Public');
+  //   await page.getByLabel('Contract Type').selectOption('Field School');
+  //   await page.getByLabel('Contract Type').selectOption('Research');
+  //   await page.getByText('licence #:', { exact: true }).click();
+  //   await page.getByText('Description', { exact: true }).click();
+  //   await page.getByRole('button', { name: 'Cancel' }).click();
+    
 
 
--- webapp/tests/e2e/1addUserTest.test.ts
+++ webapp/tests/e2e/1addUserTest.test.ts
@@ -1,38 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-  test.describe('Login and basic UI structure', () => {
-    test('Invalid login test', async ({ page }) => {
-        await test.step('load page', async () => {
-          await page.goto('http://45.77.59.59/');
-        });        
-      await test.step('login with incorrect email', async () => {
-        const emailInput = await page.getByPlaceholder('Email Addresss');
-        await expect(emailInput).toBeEmpty();
-        await emailInput.click();
-        await emailInput.type('edward@t.com');
+  test.describe('Login and add a new user', () => {
+    test('Base UI structure', async ({ page }) => {
+      await test.step('load page', async () => {
+        await page.goto('baseURL' );
       });
-
-      await test.step('type in the incorrect password', async () => {
-        const passwordInput = await page.getByPlaceholder('Password...');
-        await expect(passwordInput).toBeEmpty();
-        await passwordInput.hover();
-        await passwordInput.click();
-        await passwordInput.type('FAKEPASSWORD');
-
+  
+      await test.step('login with correct email and password', async () => {
+        await page.getByPlaceholder('Email Addresss').click();
+        await page.getByPlaceholder('Email Addresss').type('edwardtest@test.com');
+        await page.getByPlaceholder('Password...').click();
+        await page.getByPlaceholder('Password...').type('Yc(bLS*od+iX');
+        await page.getByRole('button', { name: 'Login' }).click();
       });
-
-      await test.step('click the login button', async () => {
-        const loginBtn = await page.getByRole('button', { name: 'Login' });
-        await loginBtn.hover();
-        await loginBtn.click();
-      });
-
-        await test.step('confim toast message', async () => {
-          const loginToast = await page.getByText('Invalid Login')
-          await expect(loginToast).toBeVisible()
-          await expect(loginToast).toHaveText('Invalid Login')
-          
-        });
-
+  
+    
     });
-});
+  });
