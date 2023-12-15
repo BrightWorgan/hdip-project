48e5c36ab230f6d9f2c792750a72ab81a3107229
Sarah H <sarah@cleancodecompany.com>
2023-12-15 08:11:03 +0000

    feat(api): added a unit test for hashing and edited intergration tests

--- api/test/context.intergration.test.ts
+++ api/test/context.intergration.test.ts
@@ -1,77 +1,87 @@
-import projectController from "../src/controllers/project.controller"
-import contextController from "../src/controllers/context.controller"
-import {getMockRes, getMockReq} from "@jest-mock/express"
-import {faker} from "@faker-js/faker"
-import db from "../src/util/knex"
+import projectController from "../src/controllers/project.controller";
+import contextController from "../src/controllers/context.controller";
+import { getMockRes, getMockReq } from "@jest-mock/express";
+import { faker } from "@faker-js/faker";
+import db from "../src/util/knex";
+import { createRandomUserThroughAPI } from "./team.intergration.test";
+import { createRandomProjectThroughAPI } from "./project.intergration.test";
+
+export const createRandomContextThroughAPI = async () => {
+  const user = await createRandomUserThroughAPI();
+  const project = await createRandomProjectThroughAPI();
+
+  const projectID = project.projectID;
+  const type = 'Cut';
+  const description = faker.word.words();
+  const soilType = faker.person.fullName();
+  const samples = "Yes";
+  const location = faker.location.city();
+  const date = faker.date.future();
+  const userID = user.userID;
+
+  const context = await createContextThroughAPI({
+    projectID,
+    type,
+    description,
+    soilType,
+    samples,
+    location,
+    date,
+    userID,
+  });
+  return context;
+};
+const createContextThroughAPI = async (data: any) => {
+  const ctxReq = getMockReq({
+    params: {
+        projectID: data.projectID,
+    },
+    body: data,
+  });
+
+  const ctxRes = getMockRes();
+
+  await contextController.create(ctxReq, ctxRes.res);
+
+  const ctxResult = await db("Context")
+    .where({ description: data.description })
+    .first();
+  return ctxResult;
+};
 
 describe("Project", () => {
-    it("Correctly saves a project", async() => {
-        const name = faker.company.name();
-        const projectLocation = faker.location.city();
-        const director = faker.person.fullName();
-        const startDate = faker.date.future();
-        const description = faker.word.words();
-        const contract = "Public";
-        const licenceNumber = faker.string.uuid();
+  it("Correctly saves a project", async () => {
+    const user = await createRandomUserThroughAPI();
+    const project = await createRandomProjectThroughAPI();
 
-        const req = getMockReq({  
-            body:{
-                name, 
-                projectLocation,
-                director,
-                startDate, 
-                description, 
-                contract, 
-                licenceNumber
-            }
-        });
+    const projectID = project.projectID;
+    const type = 'Cut';
+    const description = faker.word.words();
+    const soilType = faker.person.fullName();
+    const samples = "Yes";
+    const location = faker.location.city();
+    const date = faker.date.future();
+    const userID = user.userID;
 
-        const res = getMockRes();
+    const ctxResult = await createContextThroughAPI({
+      projectID,
+      type,
+      description,
+      soilType,
+      samples,
+      location,
+      date,
+      userID,
+    });
+    expect(ctxResult).not.toBeUndefined();
 
-        await projectController.create(req, res.res);
-        
-        
-    })
-
-    it("Correctly saves a context within the above project", async() =>{
-        // Context needs: type, description, projectID, soilType, samples,location, date,  userID
-
-        const projectID = faker.location.city();
-        const type = faker.company.name();
-        const description = faker.word.words();
-        const soilType = faker.person.fullName();
-        const samples = "Yes";
-        const location = faker.location.city();
-        const date = faker.date.future();
-        const userID = faker.string.uuid();
-        
-        const req = getMockReq({  
-            body:{
-                projectID,
-                type, 
-                description, 
-                soilType,
-                samples, 
-                location,
-                date, 
-                userID 
-            }
-        });
-
-        const res = getMockRes();
-
-        await contextController.create(req, res.res);
-
-        const result = await db("Context").where({description}).first();
-        expect(result).not.toBeUndefined();
-
-        expect(result.projectID).toBe(projectID);
-        expect(result.type).toBe(type);
-        expect(result.description).toBe(description);
-        expect(result.soilType).toBe(soilType);
-        expect(result.samples).toBe(samples);
-        expect(result.location).toBe(location);
-        expect(new Date(result.date).getUTCDate()).toBe(date.getUTCDate());
-        expect(result.userID).toBe(userID);
-    })
+    expect(ctxResult.projectID).toBe(projectID);
+    expect(ctxResult.type).toBe(type);
+    expect(ctxResult.description).toBe(description);
+    expect(ctxResult.soilType).toBe(soilType);
+    expect(ctxResult.samples).toBe(samples);
+    expect(ctxResult.location).toBe(location);
+    expect(new Date(ctxResult.date).getUTCDate()).toBe(date.getUTCDate());
+    expect(ctxResult.userID).toBe(userID);
+  });
 });
--- api/test/find.intergration.test.ts
+++ api/test/find.intergration.test.ts
@@ -4,97 +4,29 @@ import findsController from "../src/controllers/finds.controller"
 import {getMockRes, getMockReq} from "@jest-mock/express"
 import {faker} from "@faker-js/faker"
 import db from "../src/util/knex"
+import { createRandomUserThroughAPI } from "./team.intergration.test"
+import { createRandomProjectThroughAPI } from "./project.intergration.test"
+import { createRandomContextThroughAPI } from "./context.intergration.test"
 
-describe("Project", () => {
-    it("Correctly saves a project", async() => {
-        const name = faker.company.name();
-        const projectLocation = faker.location.city();
-        const director = faker.person.fullName();
-        const startDate = faker.date.future();
-        const description = faker.word.words();
-        const contract = "Public";
-        const licenceNumber = faker.string.uuid();
+describe("Find", () => {
 
-        const req = getMockReq({  
-            body:{
-                name, 
-                projectLocation,
-                director,
-                startDate, 
-                description, 
-                contract, 
-                licenceNumber
-            }
-        });
+    it("Correctly saves a find within the a project / context", async() =>{
+        const user = await createRandomUserThroughAPI();
+        const context = await createRandomContextThroughAPI();
 
-        const res = getMockRes();
-
-        await projectController.create(req, res.res);
-        
-        
-    })
-
-    it("Correctly saves a context within the above project", async() =>{
-        // Context needs: type, description, projectID, soilType, samples,location, date,  userID
-
-        const projectID = faker.location.city();
-        const type = faker.company.name();
-        const description = faker.word.words();
-        const soilType = faker.person.fullName();
-        const samples = "Yes";
-        const location = faker.location.city();
-        const date = faker.date.future();
-        const userID = faker.string.uuid();
-        
-        const req = getMockReq({  
-            body:{
-                projectID,
-                type, 
-                description, 
-                soilType,
-                samples, 
-                location,
-                date, 
-                userID 
-            }
-        });
-
-        const res = getMockRes();
-
-        await contextController.create(req, res.res);
-
-        const result = await db("Context").where({description}).first();
-        expect(result).not.toBeUndefined();
-
-        expect(result.projectID).toBe(projectID);
-        expect(result.type).toBe(type);
-        expect(result.description).toBe(description);
-        expect(result.soilType).toBe(soilType);
-        expect(result.samples).toBe(samples);
-        expect(result.location).toBe(location);
-        expect(new Date(result.date).getUTCDate()).toBe(date.getUTCDate());
-        expect(result.userID).toBe(userID);
-    })
-
-    it("Correctly saves a find within the above project / context", async() =>{
         // Find  needs: findNumber, contextNumber, fillNumber, description, material, photograph, bagged, date, userID
-       
-        const projectID = faker.location.city();
-        const findNumber = faker.number.int();
-        const contextNumber = faker.number.int();
-        const fillNumber = faker.number.int();
+        const contextNumber = context.contextNumber;
+        const fillNumber = faker.number.int({ max: 10000 });
         const description = faker.word.words();
         const material = faker.person.fullName();
         const photograph = "Yes";
         const bagged = "Yes";
-        const location = faker.location.city();
         const date = faker.date.future();
-        const userID = faker.string.uuid();
+        const userID = user.userID;
         
         const req = getMockReq({  
             body:{
-                projectID,
-                findNumber,
+                // findNumber,
                 contextNumber,
                 fillNumber,
                 description, 
@@ -110,11 +42,11 @@ describe("Project", () => {
 
         await findsController.create(req, res.res);
 
-        const result = await db("Context").where({description}).first();
+        expect(res.res.send).toHaveBeenCalledTimes(1);
+
+        const result = await db("Find").where({description}).first();
         expect(result).not.toBeUndefined();
 
-        expect(result.projectID).toBe(projectID);
-        expect(result.findNumber).toBe(findNumber);
         expect(result.contextNumber).toBe(contextNumber);
         expect(result.fillNumber).toBe(fillNumber);
         expect(result.description).toBe(description);
@@ -123,5 +55,41 @@ describe("Project", () => {
         expect(result.bagged).toBe(bagged);
         expect(new Date(result.date).getUTCDate()).toBe(date.getUTCDate());
         expect(result.userID).toBe(userID);
+    })
+
+    it("Correctly fails if contextNumber is not valid", async() =>{
+        const user = await createRandomUserThroughAPI();
+        const context = await createRandomContextThroughAPI();
+
+        // Find  needs: findNumber, contextNumber, fillNumber, description, material, photograph, bagged, date, userID
+    
+        const contextNumber = 'Not a valid context number';
+        const fillNumber = faker.number.int({ max: 10000 });
+        const description = faker.word.words();
+        const material = faker.person.fullName();
+        const photograph = "Yes";
+        const bagged = "Yes";
+        const date = faker.date.future();
+        const userID = user.userID;
+        
+        const req = getMockReq({  
+            body:{
+                // findNumber,
+                contextNumber,
+                fillNumber,
+                description, 
+                material,
+                photograph, 
+                bagged,
+                date, 
+                userID 
+            }
+        });
+
+        const res = getMockRes();
+
+        await findsController.create(req, res.res);
+
+        expect(res.res.status).toHaveBeenCalledTimes(1);
     })
 });
