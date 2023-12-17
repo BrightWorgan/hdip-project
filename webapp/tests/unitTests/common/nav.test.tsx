a760ba6e75fe9b00c5ae200cf2d8fb11660db91e
Sarah H <sarah@cleancodecompany.com>
2023-12-17 04:27:00 +0000

    chore: edited navbar unit test

--- webapp/tests/unitTests/common/nav.test.tsx
+++ webapp/tests/unitTests/common/nav.test.tsx
@@ -4,45 +4,29 @@ import { describe, expect, it, vi } from "vitest";
 
 import Navbar from "../../../src/common/Navbar";
 import { Tabs } from "../../../src/body";
+const testOnClick = async (name: string, tab: Tabs) => {
+  const onClick = vi.fn();
 
+  render(<Navbar tab={Tabs.Team} onClick={onClick} />);
+  const homeBtn = await screen.findAllByText(name);
+  await homeBtn[0].click();
+  expect(onClick).toHaveBeenCalled();
+  expect(onClick).toHaveBeenLastCalledWith(tab);
+};
 describe("Nav", () => {
   it("should pass enum onClick (Home)", async () => {
-    const onClick = vi.fn();
-
-    render(<Navbar tab={Tabs.Team} onClick={onClick} />);
-    const homeBtn = await screen.findAllByText("Home");
-    await homeBtn[0].click();
-    expect(onClick).toHaveBeenCalled();
-    expect(onClick).toHaveBeenLastCalledWith(Tabs.Home);
+    await testOnClick("Home", Tabs.Home);
   });
 
   it("should pass enum onClick (Team)", async () => {
-    const onClick = vi.fn();
-
-    render(<Navbar tab={Tabs.Home} onClick={onClick} />);
-    const teamBtn = await screen.findAllByText("Team");
-    await teamBtn[0].click();
-    expect(onClick).toHaveBeenCalled();
-    expect(onClick).toHaveBeenLastCalledWith(Tabs.Team);
+    await testOnClick("Team", Tabs.Team);
   });
 
-  it("should pass enum onClick (Home)", async () => {
-    const onClick = vi.fn();
-
-    render(<Navbar tab={Tabs.Team} onClick={onClick} />);
-    const homeBtn = await screen.findAllByText("Home");
-    await homeBtn[0].click();
-    expect(onClick).toHaveBeenCalled();
-    expect(onClick).toHaveBeenLastCalledWith(Tabs.Home);
+  it("should pass enum onClick (Project)", async () => {
+    await testOnClick("Project", Tabs.Project);
   });
 
-  it("should pass enum onClick (Team)", async () => {
-    const onClick = vi.fn();
-
-    render(<Navbar tab={Tabs.Home} onClick={onClick} />);
-    const teamBtn = await screen.findAllByText("Team");
-    await teamBtn[0].click();
-    expect(onClick).toHaveBeenCalled();
-    expect(onClick).toHaveBeenLastCalledWith(Tabs.Team);
+  it("should pass enum onClick (Profile)", async () => {
+    await testOnClick("User Profile", Tabs.User_Profile);
   });
 });
